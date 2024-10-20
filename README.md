5ad065a08997fb0108e9b17cd2a96391ad0f8356
Dave Henson <david.henson@bootsdigitalhealth.co.uk>
2024-10-20 14:09:35 +0100

    Increase users and add readme

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-TODO
+# Results Component - Angular
 
-- Swap files/names around 🤦
-- Raise common component styles up one level or use utility
+I need to learn Angular if I'm to move to the next level at work so this is my first attempt at applying what I've learnt from the tutorials I've been following.
+
+As ever, the difference between coding along with someone and actually doing everything myself, even on a relatively simple component like this, is a big one! NB I have really focused on Angular in this project and not worried too much about, styling or accessability.
+
+I have made a few changes to the component to enable me to use some of the Angular features I need to get the hang off. I added a few extra 'users' to the json data, this means when you click 'continue' the user should change. This addition enabled me to:
+
+* Create a 'service' to inject this data, and consolidate related manipulations
+* Pass data into the results component using an @Input()
+* Emit the user and pass into the summary component using @Output()
+* Render the results using a @for directive
+* Render the total score and associated text using an @if directive
+* Create a 'User' inferface which I could then use in multiple places to check types
+
+## Things to focus on in the future:
+
+* Increase fluency passing data between components
+* Improve understanding of Typescript, especially with regard to classes
+* Revise JS classes, with focus on the role of the constructor
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from './user.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class UsersService {
   constructor() {}
@@ -12,24 +12,24 @@ export class UsersService {
       id: 1,
       results: [
         {
-          category: 'reaction',
+          category: "reaction",
           score: 80,
-          icon: './assets/images/icon-reaction.svg',
+          icon: "./assets/images/icon-reaction.svg",
         },
         {
-          category: 'memory',
+          category: "memory",
           score: 92,
-          icon: './assets/images/icon-memory.svg',
+          icon: "./assets/images/icon-memory.svg",
         },
         {
-          category: 'verbal',
+          category: "verbal",
           score: 61,
-          icon: './assets/images/icon-verbal.svg',
+          icon: "./assets/images/icon-verbal.svg",
         },
         {
-          category: 'visual',
+          category: "visual",
           score: 72,
-          icon: './assets/images/icon-visual.svg',
+          icon: "./assets/images/icon-visual.svg",
         },
       ],
     },
@@ -37,24 +37,24 @@ export class UsersService {
       id: 2,
       results: [
         {
-          category: 'reaction',
+          category: "reaction",
           score: 20,
-          icon: './assets/images/icon-reaction.svg',
+          icon: "./assets/images/icon-reaction.svg",
         },
         {
-          category: 'memory',
+          category: "memory",
           score: 32,
-          icon: './assets/images/icon-memory.svg',
+          icon: "./assets/images/icon-memory.svg",
         },
         {
-          category: 'verbal',
+          category: "verbal",
           score: 41,
-          icon: './assets/images/icon-verbal.svg',
+          icon: "./assets/images/icon-verbal.svg",
         },
         {
-          category: 'visual',
+          category: "visual",
           score: 12,
-          icon: './assets/images/icon-visual.svg',
+          icon: "./assets/images/icon-visual.svg",
         },
       ],
     },
@@ -62,49 +62,124 @@ export class UsersService {
       id: 3,
       results: [
         {
-          category: 'reaction',
+          category: "reaction",
           score: 50,
-          icon: './assets/images/icon-reaction.svg',
+          icon: "./assets/images/icon-reaction.svg",
         },
         {
-          category: 'memory',
+          category: "memory",
           score: 42,
-          icon: './assets/images/icon-memory.svg',
+          icon: "./assets/images/icon-memory.svg",
         },
         {
-          category: 'verbal',
+          category: "verbal",
           score: 60,
-          icon: './assets/images/icon-verbal.svg',
+          icon: "./assets/images/icon-verbal.svg",
         },
         {
-          category: 'visual',
+          category: "visual",
           score: 74,
-          icon: './assets/images/icon-visual.svg',
+          icon: "./assets/images/icon-visual.svg",
         },
       ],
     },
     {
-      id: 3,
+      id: 4,
       results: [
         {
-          category: 'reaction',
-          score: 98,
-          icon: './assets/images/icon-reaction.svg',
+          category: "reaction",
+          score: 58,
+          icon: "./assets/images/icon-reaction.svg",
         },
         {
-          category: 'memory',
-          score: 87,
-          icon: './assets/images/icon-memory.svg',
+          category: "memory",
+          score: 47,
+          icon: "./assets/images/icon-memory.svg",
         },
         {
-          category: 'verbal',
+          category: "verbal",
+          score: 63,
+          icon: "./assets/images/icon-verbal.svg",
+        },
+        {
+          category: "visual",
+          score: 49,
+          icon: "./assets/images/icon-visual.svg",
+        },
+      ],
+    },
+    {
+      id: 5,
+      results: [
+        {
+          category: "reaction",
+          score: 57,
+          icon: "./assets/images/icon-reaction.svg",
+        },
+        {
+          category: "memory",
+          score: 27,
+          icon: "./assets/images/icon-memory.svg",
+        },
+        {
+          category: "verbal",
           score: 89,
-          icon: './assets/images/icon-verbal.svg',
+          icon: "./assets/images/icon-verbal.svg",
         },
         {
-          category: 'visual',
-          score: 92,
-          icon: './assets/images/icon-visual.svg',
+          category: "visual",
+          score: 34,
+          icon: "./assets/images/icon-visual.svg",
+        },
+      ],
+    },
+    {
+      id: 6,
+      results: [
+        {
+          category: "reaction",
+          score: 34,
+          icon: "./assets/images/icon-reaction.svg",
+        },
+        {
+          category: "memory",
+          score: 78,
+          icon: "./assets/images/icon-memory.svg",
+        },
+        {
+          category: "verbal",
+          score: 26,
+          icon: "./assets/images/icon-verbal.svg",
+        },
+        {
+          category: "visual",
+          score: 62,
+          icon: "./assets/images/icon-visual.svg",
+        },
+      ],
+    },
+    {
+      id: 7,
+      results: [
+        {
+          category: "reaction",
+          score: 72,
+          icon: "./assets/images/icon-reaction.svg",
+        },
+        {
+          category: "memory",
+          score: 62,
+          icon: "./assets/images/icon-memory.svg",
+        },
+        {
+          category: "verbal",
+          score: 34,
+          icon: "./assets/images/icon-verbal.svg",
+        },
+        {
+          category: "visual",
+          score: 52,
+          icon: "./assets/images/icon-visual.svg",
         },
       ],
     },
@@ -114,7 +189,7 @@ export class UsersService {
     return Math.round(
       user.results.reduce((acc, cur) => {
         return (acc += cur.score);
-      }, 0) / 4,
+      }, 0) / 4
     );
   }
 
